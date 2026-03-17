5a79980f482f66c6fb0fb39b0f5e682f074945b0
ianMonteiro07 <ian.monteiro2018@gmail.com>
2026-03-17 18:58:11 -0300

    Clean up comments and enhance code organization

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 import warnings
 
-# Silencia avisos de SSL chatos do Mac
+
 warnings.filterwarnings("ignore", category=UserWarning, module='urllib3')
 
-# --- 1. MODELO (Engenharia de Software) ---
+
 class Usuario:
     def __init__(self, id, name, email):
         self.id = id
@@ -17,10 +17,10 @@ class Postagem:
         self.id = id
         self.title = title
 
-# --- 2. CONTROLLER (Lógica e Resiliência) ---
+
 def buscar_dados():
     try:
-        # Requisitos: Verbos HTTP GET e Timeouts para Resiliência
+        
         print(" [Sincronizando com API Cloud...] ")
         u_res = requests.get("https://jsonplaceholder.typicode.com/users", timeout=5)
         p_res = requests.get("https://jsonplaceholder.typicode.com/posts", timeout=5)
@@ -32,7 +32,7 @@ def buscar_dados():
         print(f"Erro de Conexão: {e}")
         return None, None, None
 
-# --- 3. VIEW (Interface de Saída) ---
+
 def renderizar_dashboard():
     users_j, posts_j, comms_j = buscar_dados()
     
@@ -43,16 +43,16 @@ def renderizar_dashboard():
     print(f"{'ID':<3} | {'COLABORADOR':<20} | {'ÚLTIMO POST':<35} | {'CMTS'}")
     print("-" * 75)
 
-    for u in users_j[:10]: # Protótipo com os 10 primeiros
-        # Instanciando o Model
+    for u in users_j[:10]: 
+       
         user = Usuario(u['id'], u['name'], u['email'])
         
-        # Filtrando Relacionamento 1:N (Associação)
+        
         user_post = next((p for p in posts_j if p['userId'] == user.id), None)
         
         if user_post:
             post_obj = Postagem(user_post['userId'], user_post['id'], user_post['title'])
-            # Contando comentários do post
+            
             qtd_c = len([c for c in comms_j if c['postId'] == post_obj.id])
             titulo = post_obj.title[:33] + ".."
         else:
